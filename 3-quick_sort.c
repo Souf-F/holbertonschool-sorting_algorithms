412fedf9c4a84ac06e59c3246f53e0c26f60e4b1
Mr.Robot <sfecom.31000@gmail.com>
2026-04-09 16:35:53 +0200

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -65,8 +65,8 @@ void quick_recursion(int *array, size_t size, int low, int high)
  */
 void quick_sort(int *array, size_t size)
 {
-	if (!array || size < 2)
+	if (array == NULL || size < 2)
 		return;
 
-	quick_recursion(array, size, 0, size - 1);
+	quick_recursion(array, size, 0, (int)size - 1);
 }
